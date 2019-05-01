b8c5b5747654c6b47a54b2345baa4e2277bab488
Tamara <TJSXZ4@mst.edu>
2019-05-01 17:49:03 -0500

    here we go again

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mstrobotics/MATE_2019/catkin_ws/src
-# Build directory: /home/mstrobotics/MATE_2019/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
