67a3f4e63ec01684ebd658c4c44eeab8be074b5c
Joe Flynn <JoeF140@users.noreply.github.com>
2019-09-08 15:15:17 -0500

    Initial Commit

---
+++ MATE_2019/Arduino/main/pins.h
@@ -0,0 +1,28 @@
+/**
+ *Author: 	Vinnie Marco
+ *Date:		9-22-2017
+ *Email: 	vgmcn3@mst.edu
+ *
+ * 
+ *Description: 	This file defines pin names and numbers 
+ *   		for use in a .ino file
+ */
+
+
+#ifndef PINS_H
+#define PINS_H
+
+//servo and thruster pins
+#define flt_pin 4
+#define middle_left_pin 5
+#define blt_pin 2//6
+#define frt_pin 3//9
+#define middle_right_pin 6//10
+#define brt_pin 7//12
+#define back_pin 8//13
+
+//data collection pins
+#define temp_pin A0
+#define temp_sensor_pin A1
+
+#endif
