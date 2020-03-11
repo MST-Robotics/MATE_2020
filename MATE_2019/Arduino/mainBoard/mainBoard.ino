3d8b294ab4eb112a2e2cf610fd89506556cc67a0
Joey Flynn <JoeF140@users.noreply.github.com>
2020-03-11 13:15:15 -0700

    Average out IMU data, Some setup for using Sensor Fusion

--- MATE_2019/Arduino/mainBoard/mainBoard.ino
+++ MATE_2019/Arduino/mainBoard/mainBoard.ino
@@ -5,13 +5,25 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <SparkFunLSM9DS1.h>
+#include <SensorFusion.h>
 
 #define LSM9DS1_M 0x1E   // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG 0x6B  // Would be 0x6A if SDO_AG is LOW
 
-#define SerialConnection Serial
+#define SerialConnection Serial1
 
 LSM9DS1 imu;
+SF imuFusion;
+
+const int imuReadingNum = 5;
+
+float imuReadings[imuReadingNum*2];
+int imuReadIndex = 0;
+
+float pitchTotal = 0.0f;
+float rollTotal = 0.0f;
+
+int loops = 0;
 
 const int buzzer = 17;
 const int water1 = 20;
@@ -28,6 +40,10 @@ String disabledCommand = ":1500;1500;1500;1500;1500;1500;1500;1500;1500;1500;0";
 
 void setup()
 {
+  for (int i = 0; i < imuReadingNum * 2; ++i)
+  {
+    imuReadings[i] = 0.0f;
+  }
   
   delay(1000); 
 
@@ -97,23 +113,70 @@ void loop()
   float ay = 0.0;
   float az = 0.0;
 
+  float gx = 0.0;
+  float gy = 0.0;
+  float gz = 0.0;
+
+  float mx = 0.0;
+  float my = 0.0;
+  float mz = 0.0;
+
+  float deltaT;
+
   if (imu.accelAvailable())
   {
-    // Updates ax, ay, and az
+    // Updates Accelerometer data
     imu.readAccel();
-    ax = imu.ax;
-    ay = imu.ay;
-    az = imu.az;
+    ax = imu.ax;//imu.calcAccel(imu.ax);
+    ay = imu.ay;//imu.calcAccel(imu.ay);
+    az = imu.az;//imu.calcAccel(imu.az);
   }
 
-  //if (imu.gyroAvailable())
+  if (imu.gyroAvailable())
   {
-    // Updates gx, gy, and gz
-    //imu.readGyro();
+    // Updates Gyroscope data
+    imu.readGyro();
+    gx = imu.calcGyro(imu.gx) * DEG_TO_RAD;
+    gy = imu.calcGyro(imu.gy) * DEG_TO_RAD;
+    gz = imu.calcGyro(imu.gz) * DEG_TO_RAD;
   }
 
-  float roll = atan2(ay, az) * 180 / PI;
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180 / PI;
+  if (imu.magAvailable())
+  {
+    // Updates Compass data
+    imu.readMag();
+    mx = imu.mx * 0.1;
+    my = imu.my * 0.1;
+    mz = imu.mz * 0.1;
+  }
+
+  deltaT = imuFusion.deltatUpdate();
+  imuFusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltaT);
+
+  pitchTotal -= imuReadings[imuReadIndex];
+  imuReadings[imuReadIndex] = atan2(-ax, sqrt(ay * ay + az * az)) * RAD_TO_DEG;//imuFusion.getPitch();
+  pitchTotal += imuReadings[imuReadIndex];
+
+  rollTotal -= imuReadings[imuReadIndex+1];
+  imuReadings[imuReadIndex+1] = atan2(ay, az) * RAD_TO_DEG;//imuFusion.getRoll();
+  rollTotal += imuReadings[imuReadIndex+1];
+
+  imuReadIndex += 2;
+  if (imuReadIndex >= imuReadingNum*2) 
+  {
+    imuReadIndex = 0;
+  }
+
+  float pitch = pitchTotal / imuReadingNum;
+  float roll = rollTotal / imuReadingNum;
+
+  //float pitch = imuFusion.getPitch();
+  //float roll = imuFusion.getRoll();
+
+  //float roll = atan2(ay, az) * RAD_TO_DEG;
+  //float pitch = atan2(-ax, sqrt(ay * ay + az * az)) * RAD_TO_DEG;
+
+  ++loops;
 
   char cstr[16];
   itoa(timer, cstr, 10);
@@ -125,16 +188,8 @@ void loop()
     if (SerialConnection.read() == ':')
     {
 	    timer = 0;  // Reset timer if valid data received
-
       // Only send data back if data was received
-//      if (digitalRead(water4) && digitalRead(water3) && digitalRead(water2) && digitalRead(water1))
-//      {
-//        writeString("0");
-//      }
-//      else
-//      {
-//        writeString("1");
-//      }
+      
       String info;
       info = disabledCommand;
 
@@ -143,18 +198,26 @@ void loop()
       info.toCharArray(driveCommands, COMMAND_SIZE - 1);
       drive(driveCommands);
 
-      writeString(":");
-      if (roll < 10.0 && roll > 0.0)
+      // Send back Water Sensor data
+      if (digitalRead(water4) && digitalRead(water3) && digitalRead(water2) && digitalRead(water1))
       {
-        writeString("0");
+        writeString("0;");
       }
-      writeString(String(roll, 3));
+      else
+      {
+        writeString("1;");
+      }
+
+
+      // Send back IMU data
+      writeString(String(pitch,2));
       writeString(";");
-      if (pitch < 10.0 && pitch > 0.0)
-      {
-        writeString("0");
-      }
-      writeString(String(pitch, 3));
+      writeString(String(roll,2));
+
+      writeString(";");
+      writeString(String(loops));
+
+      loops = 0;
 
       digitalWrite(buzzer, 1);
 
